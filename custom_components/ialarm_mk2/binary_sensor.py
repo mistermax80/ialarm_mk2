2dd7f52b4b602a285905154684eeeeaa8c3a64b1
Mistermax80 <giglimassimo@gmail.com>
2025-07-26 23:44:51 +0200

    test interno ok

--- custom_components/ialarm_mk2/binary_sensor.py
+++ custom_components/ialarm_mk2/binary_sensor.py
@@ -20,6 +20,7 @@ from .const import DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
 
+
 async def async_setup_entry(
     hass: HomeAssistant, entry: ConfigEntry, async_add_entities: AddEntitiesCallback
 ) -> None:
@@ -27,21 +28,26 @@ async def async_setup_entry(
     _LOGGER.info("Set up sensors based on a config entry.")
     coordinator: DataUpdateCoordinator = hass.data[DOMAIN][entry.entry_id]
     if hasattr(coordinator, "sensors"):
-        async_add_entities(coordinator.sensors,True)
-    _LOGGER.debug(coordinator.sensors)
+        async_add_entities(coordinator.sensors, True)
+    _LOGGER.debug(
+        "Set up %d sensors: %s",
+        len(coordinator.sensors),
+        [s.name for s in coordinator.sensors],
+    )
+
 
 class IAlarmmkSensor(CoordinatorEntity, BinarySensorEntity):
     """Representation of a iAlarm Status Sensor."""
 
     def __init__(
         self,
-        coordinator: DataUpdateCoordinator,
+        coordinator: CoordinatorEntity,
         device: DeviceInfo,
         name: str,
         index: int,
         entity_id: str,
         unique_id: str,
-        zone_type: int
+        zone_type: int,
     ) -> None:
         """Initialize the sensor."""
         super().__init__(coordinator)
@@ -50,12 +56,12 @@ class IAlarmmkSensor(CoordinatorEntity, BinarySensorEntity):
         self._attr_name = name
         self._attr_entity_id = entity_id
         self._attr_index: int = index
-        #Types: 0: Disabilitata, 1: Ritardata, 2: Perimetrale, 3:Interna, 4: Emergenza, 5: Attiva 24 ore, 6: Incendio, 7: Chiavi
+        # Types: 0: Disabilitata, 1: Ritardata, 2: Perimetrale, 3:Interna, 4: Emergenza, 5: Attiva 24 ore, 6: Incendio, 7: Chiavi
         match zone_type:
             case 1 | 2:
                 if "port" in name.lower():
                     self._attr_device_class = BinarySensorDeviceClass.DOOR
-                elif("intern" in name.lower()):
+                elif "intern" in name.lower():
                     self._attr_device_class = BinarySensorDeviceClass.MOTION
                 else:
                     self._attr_device_class = BinarySensorDeviceClass.WINDOW
@@ -64,38 +70,35 @@ class IAlarmmkSensor(CoordinatorEntity, BinarySensorEntity):
             case 4 | 5:
                 self._attr_device_class = BinarySensorDeviceClass.PROBLEM
             case 6:
-                if("gas" in name.lower()):
+                if "gas" in name.lower():
                     self._attr_device_class = BinarySensorDeviceClass.GAS
                 else:
                     self._attr_device_class = BinarySensorDeviceClass.SMOKE
             case 0 | _:
                 self._attr_device_class = BinarySensorDeviceClass.OPENING
         self._attr_is_on = None
-        self._attr_low_battery:bool = None
-        self._attr_loss:bool = None
-        self._attr_bypass:bool = None
+        self._attr_low_battery: bool = None
+        self._attr_loss: bool = None
+        self._attr_bypass: bool = None
         self._attr_last_check = None
-
-    def set_attr_is_on(self, state:bool):
-        '''set_attr_is_on.'''
-        self._attr_is_on = state
 
     @property
     def is_on(self) -> bool | None:
-        '''is_on.'''
-        return self._attr_is_on
+        """Return whether the sensor is on."""
+        #_LOGGER.debug("Getting is_on for sensor index: %s", self._attr_index)
+        self._sensor_map = {s.index: s for s in self.coordinator.data.sensors_data}
+        sensor = self._sensor_map.get(self._attr_index)
+        return sensor.is_on if sensor else None
 
     @property
-    def index(self) -> bool | None:
-        '''is_on.'''
+    def index(self) -> int:
+        """Return sensor index."""
         return self._attr_index
 
-    def set_state(self, state):
-        '''set_state.'''
-        self._attr_state = state
-
-    def set_extra_state_attributes(self,low_battery:bool,loss:bool,bypass:bool,last_check):
-        '''set_extra_state_attributes.'''
+    def set_extra_state_attributes(
+        self, low_battery: bool, loss: bool, bypass: bool, last_check
+    ):
+        """set_extra_state_attributes."""
         self._low_battery = low_battery
         self._loss = loss
         self._bypass = bypass
